f965c2be30037ccf2a779573271c4a226bf53762
Alexej Hein <alexej.hein@aol.de>
2024-04-13 14:11:11 +0200

    add login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { AuthService } from "../services/auth.service";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -16,13 +17,15 @@ export class LoginComponent {
   username: string | undefined;
   password: string | undefined;
 
-  constructor(private as:AuthService) { }
+  constructor(private as:AuthService, private router: Router) { }
 
   async login() {
     try{
       let response = await this.as.loginWithHttpClient(this.username!, this.password!);
+      await this.router.navigate(['/todos']);
       console.log(response);
     } catch (error) {
+      alert("Login failed");
       console.error(error);
     }
   }
